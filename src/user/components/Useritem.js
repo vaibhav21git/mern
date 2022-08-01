47ff6013238cb7b1ac880b1fa6a34b4e4b73d8fc
vaibhav <vaibhavsachdeva2000@gmail.com>
2022-08-01 10:08:25 +0530

    Styling our useritem components by adding  card and avatar

--- src/user/components/Useritem.js
+++ src/user/components/Useritem.js
@@ -1,20 +1,28 @@
-import React from 'react'
-import './Useritem.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Useritem(props) {
+import Avatar from '../../shared/components/UIelements/Avatar';
+import Card from '../../shared/components/UIelements/Card';
+import './Useritem.css';
+
+const Useritem = props => {
   return (
-    <li className='user-item'>
-     <div className='user-item__content'>
-        <div className='user-item__image'>
-          <img src = {props.image} alt = {props.name}/>
-        </div>
-        <div className='user-item__info'>
+    <li className="user-item">
+      <Card className="user-item__content">
+        <Link to={`/${props.id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item__info">
             <h2>{props.name}</h2>
-            <h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}</h3>
-        </div>
-     </div>
+            <h3>
+              {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
+            </h3>
+          </div>
+        </Link>
+      </Card>
     </li>
-  )
-}
+  );
+};
 
-export default Useritem
+export default Useritem;
